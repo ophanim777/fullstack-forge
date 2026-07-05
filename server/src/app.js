e48b2366d54a756abe6662e0463235e34d11e4b5
ophanim777 <rikyjo49@gmail.com>
2026-07-05 22:44:37 +0700

    feat : move all routes to routes folder

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
+import routes from "./routes/index.js";
+
 const app = express();
 
 
@@ -12,12 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-
-app.get("/api/health", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Server is running 🚀",
-  });
-});
+app.use("/api", routes);
 
 export default app;
