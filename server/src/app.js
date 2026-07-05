f237421e94aa139f1fb0989c93507d57d5b16d98
ophanim777 <rikyjo49@gmail.com>
2026-07-05 22:25:46 +0700

    feat : add express app, activate middleware cors, helmet and morgan, and export express instance

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running 🚀",
+  });
+});
+
+export default app;
