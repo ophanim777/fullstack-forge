f0a16ec6114031e22639821d49218d7337d3b1df
ophanim777 <rikyjo49@gmail.com>
2026-07-12 15:33:53 +0700

    feat(server): create authentication controller

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import { registerSchema } from "../validators/auth.validator.js";
+import { registerUser } from "../services/auth.service.js";
+
+export async function register(req, res, next) {
+  try {
+    const body = registerSchema.parse(req.body);
+
+    const user = await registerUser(body);
+
+    res.status(201).json({
+      success: true,
+      message: "User berhasil dibuat.",
+      user: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+}
