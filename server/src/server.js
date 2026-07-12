9393bd26dd145262fae993e3307bcc9b687b4eec
ophanim777 <rikyjo49@gmail.com>
2026-07-12 15:58:25 +0700

    fix(server): fixing server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,6 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 
-
-const PORT = process.env.PORT || 3000;
-
 app.listen(env.PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
