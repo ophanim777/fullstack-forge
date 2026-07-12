0877c98d72f9e08a7532898f62104191d27b4ad0
ophanim777 <rikyjo49@gmail.com>
2026-07-12 20:52:27 +0700

    feat(server): add access token and refresh token expire time

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -4,4 +4,6 @@ export const env = {
   PORT: process.env.PORT || 5000,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  ACCESS_TOKEN_EXPIRES: process.env.ACCESS_TOKEN_EXPIRES || "15m",
+  REFRESH_TOKEN_EXPIRES: process.env.REFRESH_TOKEN_EXPIRES || "7d",
 };
