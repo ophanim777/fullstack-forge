5d90f4dab5ea37aad510d0314328456d8f5f85d3
ophanim777 <rikyjo49@gmail.com>
2026-07-19 13:24:09 +0700

    feat(server): add update avatar routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
 import { updateUserProfile } from "../controllers/user.controller.js";
+import { upload } from "../middleware/upload.middleware.js";
+import { uploadAvatar } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.put("/profile", authenticate, updateUserProfile);
+
+router.post(
+  "/avatar",
+  authenticate,
+  upload.single("avatar"),
+  uploadAvatar
+);
 
 export default router;
