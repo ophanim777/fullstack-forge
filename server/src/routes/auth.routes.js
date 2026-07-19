c1a34f5cdf27cf17f13fcbef435bafab98be2256
ophanim777 <rikyjo49@gmail.com>
2026-07-19 10:46:33 +0700

    feat(server): add refresh route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { register, login, getMe, } from "../controllers/auth.controller.js";
+import { register, login, getMe, refresh, } from "../controllers/auth.controller.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/refresh", refresh);
 
 router.get("/me", authenticate, getMe);
 
