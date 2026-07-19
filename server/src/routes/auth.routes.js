8725cb8f8f4a5c0bdf799adbc2b8e42435ede918
ophanim777 <rikyjo49@gmail.com>
2026-07-19 10:57:14 +0700

    feat(server): add logout route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login, getMe, refresh, } from "../controllers/auth.controller.js";
+import { register, login, getMe, refresh, logout, } from "../controllers/auth.controller.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 
 const router = Router();
@@ -7,6 +7,7 @@ const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/refresh", refresh);
+router.post("/logout", logout);
 
 router.get("/me", authenticate, getMe);
 
