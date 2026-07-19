47adfca585f9f09a5a960e4668059e9220423f38
ophanim777 <rikyjo49@gmail.com>
2026-07-19 22:10:18 +0700

    feat(server): add get single post routes

--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
-import { createPost, getPosts, } from "../controllers/post.controller.js";
+import { createPost, 
+    getPosts,  
+    getPost,} from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.get("/", getPosts);
+
+router.get("/:id", getPost);
 
 router.post("/", authenticate, createPost);
 
