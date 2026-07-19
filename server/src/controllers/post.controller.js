80165ae1c4980f7ee50ad5ecc3bdaea6970b82b1
ophanim777 <rikyjo49@gmail.com>
2026-07-19 14:05:35 +0700

    feat(server): add post controller

---
+++ server/src/controllers/post.controller.js
@@ -0,0 +1,18 @@
+import { createPostSchema } from "../validators/post.validator.js";
+import { createPost as createPostService } from "../services/post.service.js";
+
+export async function createPost(req, res, next) {
+  try {
+    const body = createPostSchema.parse(req.body);
+
+    const post = await createPostService(req.user.id, body);
+
+    res.status(201).json({
+      success: true,
+      message: "Post berhasil dibuat.",
+      post,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
