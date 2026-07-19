8c4da6a383e2c2515e2c6d79fccbe909378f2aa2
ophanim777 <rikyjo49@gmail.com>
2026-07-19 22:14:34 +0700

    feat(server): add update post validator

--- server/src/validators/post.validator.js
+++ server/src/validators/post.validator.js
@@ -7,3 +7,11 @@ export const createPostSchema = z.object({
     .min(1, "Konten tidak boleh kosong.")
     .max(1000, "Konten maksimal 1000 karakter."),
 });
+
+export const updatePostSchema = z.object({
+  content: z
+    .string()
+    .trim()
+    .min(1, "Content wajib diisi.")
+    .max(1000, "Content maksimal 1000 karakter."),
+});
