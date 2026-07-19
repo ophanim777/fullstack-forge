5a4c9ecd6f04554dfaca3749cf6372ed6b7ebf53
ophanim777 <rikyjo49@gmail.com>
2026-07-19 14:08:02 +0700

    feat(server): add post routes in index

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import healthRoutes from "./health.routes.js";
 import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
+import postRoutes from "./post.routes.js";
 
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
 
 export default router;
